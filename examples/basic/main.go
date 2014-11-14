55996315c6968f2613985c486ef78363dba4b1cf
Willem van der Schyff <willemvds@gmail.com>
2014-11-14 19:18:09 +0200

    Make the test app make more sense and less of a pain

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -2,36 +2,43 @@ package main
 
 import (
 	"flag"
-	"log"
+	//"log"
 	"os"
 	"strings"
 
 	"github.com/willemvds/Implodatron"
 )
 
+/*
 var path string
 
 func init() {
 	flag.StringVar(&path, "path", "", "path to 'main' file")
 }
+*/
 
 func main() {
 	flag.Parse()
-	if path == "" {
-		log.Fatalf("Usage: basic -path <myapp.py>")
-	}
-	var pathroot string
-	sidx := strings.LastIndex(path, "/")
-	if sidx != -1 {
-		sidx++
-		pathroot = path[0:sidx]
-		path = path[sidx:]
-	} else {
-		pathroot, _ = os.Getwd()
-		pathroot = pathroot + "/"
-	}
+	/*
+		if path == "" {
+			log.Fatalf("Usage: basic -path <myapp.py>")
+		}
+	*/
 
-	root := implodatron.BuildTree(path, []string{pathroot})
-	root.Print()
-	print("\n")
+	for _, path := range flag.Args() {
+		var pathroot string
+		sidx := strings.LastIndex(path, "/")
+		if sidx != -1 {
+			sidx++
+			pathroot = path[0:sidx]
+			path = path[sidx:]
+		} else {
+			pathroot, _ = os.Getwd()
+			pathroot = pathroot + "/"
+		}
+
+		root := implodatron.BuildTree(path, []string{pathroot})
+		root.Print()
+		print("\n")
+	}
 }
